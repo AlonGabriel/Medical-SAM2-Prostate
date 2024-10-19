7e7ba7b7cf1ec62344732869e00edff404cb13a7
Alon Gabriel <alongabriel10@gmail.com>
2024-10-19 15:48:21 -0400

    Updating to add subset for smaller training

--- func_3d/dataset/__init__.py
+++ func_3d/dataset/__init__.py
@@ -2,7 +2,7 @@ from .btcv import BTCV
 from .amos import AMOS
 from .prostate_mri import ProstateMRI
 import torchvision.transforms as transforms
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 
 
 
@@ -38,9 +38,16 @@ def get_dataloader(args):
         train_dataset = ProstateMRI(args, args.data_path, transform=transform_train, transform_msk=transform_train_seg, mode='Training', prompt=args.prompt)
         test_dataset = ProstateMRI(args, args.data_path, transform=transform_train, transform_msk=transform_train_seg, mode='Test', prompt=args.prompt)
         val_dataset = ProstateMRI(args, args.data_path, transform=transform_train, transform_msk=transform_train_seg, mode='Validation', prompt=args.prompt)
-
+    
     else:
         raise ValueError("The dataset is not supported now!")
+
+    # Use a subset for debugging if needed
+    if args.debug:
+        subset_size = args.subset_size if hasattr(args, 'subset_size') else 10
+        train_dataset = Subset(train_dataset, list(range(min(len(train_dataset), subset_size))))
+        val_dataset = Subset(val_dataset, list(range(min(len(val_dataset), subset_size))))
+        test_dataset = Subset(test_dataset, list(range(min(len(test_dataset), subset_size))))
 
     # Create dataloaders
     train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=8, pin_memory=True)
